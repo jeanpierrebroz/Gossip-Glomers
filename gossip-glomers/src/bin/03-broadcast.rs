260b936ff326126d2f6e3f99b9f357411f3bca3a
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-02-18 22:44:53 -0700

    03a works!

---
+++ gossip-glomers/src/bin/03-broadcast.rs
@@ -0,0 +1,68 @@
+use maelstrom_common::{run, HandleMessage, Envelope};
+use serde::{Deserialize, Serialize};
+use core::panic;
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum Message {
+    #[serde(rename = "init")]
+    Init { msg_id: Option<usize>, node_id: String },
+    #[serde(rename = "init_ok")]
+    InitOk { in_reply_to: Option<usize> },
+
+    #[serde(rename = "broadcast")]
+    Broadcast { in_reply_to: Option<usize>, message: u32 },
+    #[serde(rename = "broadcast_ok")]
+    BroadcastOk { in_reply_to: Option<usize> },
+
+    #[serde(rename = "read")]
+    Read { msg_id: usize },
+    #[serde(rename = "read_ok")]
+    ReadOk { messages: Vec<i32>, in_reply_to: Option<usize> },
+
+    #[serde(rename = "topology")]
+    Topology { topology: std::collections::HashMap<String, Vec<String>> },
+    #[serde(rename = "topology_ok")]
+    TopologyOk { in_reply_to: Option<usize> }
+}
+
+#[derive(Debug, Default)]
+pub struct Broadcast {
+    node_id: String,
+    counter: u32
+}
+
+impl HandleMessage for Broadcast {
+    type Message = Message;
+    type Error = std::io::Error;
+
+    fn handle_message(
+        &mut self,
+        msg: Envelope<Self::Message>,
+        outbound_msg_tx: std::sync::mpsc::Sender<Envelope<Self::Message>>,
+    ) -> Result<(), Self::Error> {
+        match msg.body {
+            Message::Init { msg_id, ref node_id } => {
+                self.node_id = node_id.clone();
+                outbound_msg_tx.send(
+                    msg.reply(Message::InitOk { in_reply_to: msg_id })
+                ).unwrap();
+                Ok(())
+            },
+
+            Message::Broadcast { msg_id } => {
+                outbound_msg_tx.send(
+                    msg.reply(
+                    )
+                ).unwrap();
+                Ok(())
+            },
+            _ => panic!("{}", format!("Unexpected message: {:#?}", serde_json::to_string_pretty(&msg)))
+        }
+    }
+}
+
+fn main() {
+    let _ = run(UID::default());
+}
+
