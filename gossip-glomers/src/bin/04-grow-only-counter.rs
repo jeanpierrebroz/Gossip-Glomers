7c94245798783438df061641f93ddcda0f76c678
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-02-24 12:56:07 -0700

    can reuse this code

--- gossip-glomers/src/bin/04-grow-only-counter.rs
+++ gossip-glomers/src/bin/04-grow-only-counter.rs
@@ -1,3 +1,6 @@
+use std::{collections::HashMap, sync::mpsc::Sender, thread, time::{Duration, Instant}};
+use serde::{Deserialize, Serialize};
+
 /*
 Sequential consistency is a strong safety property for concurrent systems. 
 Informally, sequential consistency implies that operations appear to take place in some total order, 
@@ -11,14 +14,137 @@ For instance, they may read arbitrarily stale state. However, once a process A h
 from process B, it can never observe a state prior to B. 
 This, combined with the total ordering property, makes sequential consistency a surprisingly strong model 
 for programmers.
-
-
 */
+use maelstrom_common::{Envelope, HandleMessage, run};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(tag = "type")]
+pub enum Message {
+    #[serde(rename = "init")]
+    Init {
+        msg_id: Option<usize>,
+        node_id: String,
+    },
+    #[serde(rename = "init_ok")]
+    InitOk { in_reply_to: Option<usize> },
+
+    #[serde(rename = "broadcast")]
+    Broadcast { message: i32, msg_id: Option<usize> },
+    #[serde(rename = "broadcast_ok")]
+    BroadcastOk { in_reply_to: Option<usize> },
+
+    #[serde(rename = "read")]
+    Read { msg_id: usize },
+    #[serde(rename = "read_ok")]
+    ReadOk {
+        messages: Vec<i32>,
+        in_reply_to: Option<usize>,
+    },
+
+    #[serde(rename = "topology")]
+    Topology {
+        topology: HashMap<String, Vec<String>>,
+        msg_id: Option<usize>,
+    },
+    #[serde(rename = "topology_ok")]
+    TopologyOk { in_reply_to: Option<usize> },
+
+    #[serde(rename = "tick")]
+    Tick {},
+}
+
+#[derive(Debug, Clone)]
+pub struct PendingMessage {
+    message: i32,
+    time_sent: Instant,
+    dest: String,
+}
+
+#[derive(Debug, Default)]
+pub struct GrowOnlyCounter {
+    //keep track of non-acked message (similar to the broadcast challenge), and use a retry loop (also similar to broadcast)
+    //send pings to retry messages
+    node_id: String,
+    counter: usize,
+    pending_messages: HashMap<usize, PendingMessage>,
+}
 
 
-//create a key per node id?
-//
+impl HandleMessage for GrowOnlyCounter{
+    type Message = Message;
+    type Error = std::io::Error;
+
+    fn handle_message(
+        &mut self,
+        msg: Envelope<Self::Message>,
+        outbound_msg_tx: Sender<Envelope<Self::Message>>,
+    ) -> Result<(), Self::Error> {
+        match msg.body {
+            Message::Init {
+                msg_id,
+                ref node_id,
+            } => {
+                self.node_id = node_id.clone();
+                self.counter = 0;
+
+                let node_id_clone = self.node_id.clone();
+                let outbound_clone = outbound_msg_tx.clone();
+
+                thread::spawn(move || {
+                    loop {
+                        thread::sleep(Duration::from_millis(500));
+                        let _ = outbound_clone.send(Envelope {
+                            src: node_id_clone.clone(),
+                            dest: node_id_clone.clone(),
+                            body: Message::Tick {},
+                        });
+                    }
+                });
+
+                outbound_msg_tx
+                    .send(msg.reply(Message::InitOk {
+                        in_reply_to: msg_id,
+                    }))
+                    .unwrap();
+
+                Ok(())
+            }
+
+            Message::Tick {} => {
+                let now = Instant::now();
+                let timeout = Duration::from_millis(500);
+
+                for (id, pending) in &mut self.pending_messages {
+                    if now.duration_since(pending.time_sent) >= timeout {
+                        pending.time_sent = Instant::now();
+                        outbound_msg_tx
+                            .send(Envelope {
+                                src: self.node_id.clone(),
+                                dest: pending.dest.clone(),
+                                body: Message::Broadcast {
+                                    message: pending.message,
+                                    msg_id: Some(*id),
+                                },
+                            })
+                            .unwrap();
+                    }
+                }
+                Ok(())
+            }
+
+            _ => panic!(
+                "{}",
+                format!(
+                    "Unexpected message: {:#?}",
+                    serde_json::to_string_pretty(&msg)
+                )
+            ),
+        }
+    }
+}
+
+
 
 pub fn main() {
-    
+    let _ = run(GrowOnlyCounter::default());
 }
