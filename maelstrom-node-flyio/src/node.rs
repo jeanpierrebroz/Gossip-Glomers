e4f68b9c15682730bbb0c84245095f7bb3e02768
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-08 16:37:17 -0600

    more jargon

--- maelstrom-node-flyio/src/node.rs
+++ maelstrom-node-flyio/src/node.rs
@@ -5,20 +5,35 @@ use crate::protocol::envelope::Envelope;
 
 pub struct Node {
     node_id: String,
-    //keep track of outbound messages by msg id
     outbound_messages: HashMap<usize, Envelope<Value>>
 }
 
-pub fn run() {
-    //handle Init + InitOk
-    thread::spawn(move || {
-                    loop {
-                        thread::sleep(Duration::from_millis(500));
-                        let _ = outbound_clone.send(Envelope {
-                            src: node_id_clone.clone(),
-                            dest: node_id_clone.clone(),
-                            body: Message::Tick {},
-                        });
-                    }
+pub async fn run() {
+    while let Some(msg) = stream.next_message().await? {
+        let node = Arc::clone(&self);
+    
+        match msg.body {
+            // 1. Check if this is a response to an RPC we sent
+            InternalBody::ReadOk { value, in_reply_to } => {
+                let mut room = self.waiting_room.lock().await;
+                if let Some(tx) = room.remove(&in_reply_to) {
+                    let _ = tx.send(InternalBody::ReadOk { value, in_reply_to });
+                }
+            }
+    
+            // 2. Handle Handshake
+            InternalBody::Init { node_id } => {
+                node.
+            }
+    
+            // 3. Delegate User Logic
+            InternalBody::User(user_body) => {
+                let handler = Arc::clone(&self.handler);
+                tokio::spawn(async move {
+                    handler.handle_message(node, msg.replace_body(user_body)).await
                 });
+            }
+            _ => {}
+        }
+    }
 }
