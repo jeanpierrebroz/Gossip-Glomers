51471f45abd006e0f9181a4e620c1a4366ab71da
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-03-31 12:57:14 -0600

    we can read from stdin

--- gossip-glomers-rs/src/main.rs
+++ gossip-glomers-rs/src/main.rs
@@ -1,3 +1,24 @@
+use std::io::{self, BufRead};
+
 fn main() {
     println!("Hello, world!");
+    read();
+}
+
+
+
+fn read() {
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        match &line {
+            Ok(s) => {
+                println!("Read line: {:?}", s);
+            }
+
+            Err(s) => {
+                println!("Couldn't read line: {:?}", s);
+            }
+        }
+    }
 }
