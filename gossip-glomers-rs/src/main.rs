f228b2334a6203ef7a6a101e50f61289b23bd969
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-04-02 13:13:07 -0600

    rewrite tests for mapping stuff

--- gossip-glomers-rs/src/main.rs
+++ gossip-glomers-rs/src/main.rs
@@ -1,16 +1,17 @@
-use std::io::BufRead;
 use serde::{Deserialize, Serialize};
+use std::io::BufRead;
 
+use std::sync::mpsc::{Sender, channel};
 
-use std::sync::mpsc::{
-    channel,
-    Sender
-};
-
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(tag = "type", rename_all = "snake_case")]
 enum SystemType {
-    Init,
-    InitOk
-}   
+    Init {
+        node_id: String,
+        node_ids: Vec<String>,
+    },
+    InitOk,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Message<T> {
@@ -21,28 +22,25 @@ pub struct Message<T> {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Body<T> {
-    #[serde(rename = "type")]
-    pub msg_type: String,
     pub msg_id: Option<usize>,
     pub in_reply_to: Option<usize>,
     #[serde(flatten)]
     pub contents: T,
 }
 
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(tag = "type", rename_all = "snake_case")]
 struct Init {
     node_id: String,
-    node_ids: Vec<String>
+    node_ids: Vec<String>,
 }
-
 
 fn main() {
     let (sender, _recv) = channel();
     read(std::io::stdin().lock(), sender);
 }
 
-
 fn read<R: BufRead>(reader: R, sender: Sender<String>) {
-    
     for line in reader.lines() {
         match line {
             Ok(s) => {
@@ -59,33 +57,64 @@ fn read<R: BufRead>(reader: R, sender: Sender<String>) {
 
 //next tests: pass in user-defined enum and ensure correct types are sent through receiver. ensure it panics if unknown type is encountered
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
-    fn test_input() {
-        let (sender, receiver) = channel();
-        let input = "line one\nline two\n";
-        let reader = input.as_bytes();
-        read(reader, sender);
-        
-        assert_eq!(receiver.recv().unwrap(), "line one");
-        
-        assert_ne!(receiver.recv().unwrap(), "line one");
+    fn test_parse_valid_init() {
+        //real message from maelstrom
+        let input = r#"{
+            "src": "c1",
+            "dest": "n1",
+            "body": {
+                "type": "init",
+                "msg_id": 1,
+                "node_id": "n1",
+                "node_ids": ["n1", "n2"]
+            }
+        }"#;
+
+        let msg: Message<SystemType> = parse(input);
+
+        assert_eq!(msg.src, "c1");
+        match msg.body.contents {
+            SystemType::Init { ref node_id, .. } => assert_eq!(node_id, "n1"),
+            _ => panic!("Expected Init variant"),
+        }
     }
-    
+
     #[test]
-    fn test_type_mapping() {
-        let (sender, receiver) = channel();
-        let input = "line one\nline two\n";
-        let reader = input.as_bytes();
-        read(reader, sender);
-        
-        assert_eq!(receiver.recv().unwrap(), "line one");
-        
-        assert_ne!(receiver.recv().unwrap(), "line one");
+    #[should_panic]
+    fn test_parse_unknown_type_panics() {
+        //unhandled type
+        let input = r#"{
+            "src": "c1",
+            "dest": "n1",
+            "body": {
+                "type": "calculate",
+                "msg_id": 1
+            }
+        }"#;
+
+        //this should trigger the panic inside the parse function
+        let _: Message<SystemType> = parse(input);
     }
-    
+
+    #[test]
+    #[should_panic]
+    fn test_parse_malformed_json_panics() {
+        let input = r#"{"src": "broken", "body": "not_an_object"}"#;
+        let _: Message<SystemType> = parse(input);
+    }
+}
+
+fn parse<T>(message: &str) -> Message<T>
+where
+    T: serde::de::DeserializeOwned,
+{
+    match serde_json::from_str::<Message<T>>(message) {
+        Ok(msg) => msg,
+        Err(e) => panic!("Parsing failed: {}. Input was: {}", e, message),
+    }
 }
