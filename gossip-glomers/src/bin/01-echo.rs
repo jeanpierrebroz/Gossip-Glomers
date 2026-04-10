c51ba6c4af873ed6edb2ec617f00ec5255f9c529
jeanpierrebroz <jeanpierrebroz@gmail.com>
2026-04-09 22:53:04 -0600

    make echo simple

--- gossip-glomers/src/bin/01-echo.rs
+++ gossip-glomers/src/bin/01-echo.rs
@@ -1,82 +1,22 @@
-use core::panic;
-use maelstrom_common::{Envelope, HandleMessage, run};
-use serde::{Deserialize, Serialize};
+use gossip_glomers_rs::io::{Handler, Message};
+use gossip_glomers_rs::node::Node;
+use gossip_glomers_rs::protocol::Protocol;
+use gossip_glomers_rs::run;
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
-pub enum Message {
-    #[serde(rename = "init")]
-    Init {
-        #[serde(skip_serializing_if = "Option::is_none")]
-        msg_id: Option<usize>,
-        node_id: String,
-    },
-    #[serde(rename = "echo")]
-    Echo {
-        echo: String,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        msg_id: Option<usize>,
-    },
-    #[serde(rename = "init_ok")]
-    InitOk {
-        #[serde(skip_serializing_if = "Option::is_none")]
-        in_reply_to: Option<usize>,
-    },
-    #[serde(rename = "echo_ok")]
-    EchoOk {
-        echo: String,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        in_reply_to: Option<usize>,
-    },
-}
+struct EchoHandler;
 
-#[derive(Debug, Default)]
-pub struct Echo {
-    node_id: Option<String>,
-}
-
-impl HandleMessage for Echo {
-    type Message = Message;
-    type Error = std::io::Error;
-
-    fn handle_message(
-        &mut self,
-        msg: Envelope<Self::Message>,
-        outbound_msg_tx: std::sync::mpsc::Sender<Envelope<Self::Message>>,
-    ) -> Result<(), Self::Error> {
-        match msg.body {
-            Message::Init {
-                msg_id,
-                ref node_id,
-            } => {
-                self.node_id = Some(node_id.clone());
-                outbound_msg_tx
-                    .send(msg.reply(Message::InitOk {
-                        in_reply_to: msg_id,
-                    }))
-                    .unwrap();
-                Ok(())
+impl Handler<Protocol> for EchoHandler {
+    fn handle(&mut self, msg: Message<Protocol>, node: &Node) {
+        let (build_reply, contents) = msg.into_reply(node.get_next_msg_id());
+        match contents {
+            Protocol::Echo { echo } => {
+                node.send(build_reply(Protocol::EchoOk { echo }));
             }
-            Message::Echo { ref echo, msg_id } => {
-                outbound_msg_tx
-                    .send(msg.reply(Message::EchoOk {
-                        echo: echo.to_owned(),
-                        in_reply_to: msg_id,
-                    }))
-                    .unwrap();
-                Ok(())
-            }
-            _ => panic!(
-                "{}",
-                format!(
-                    "Unexpected message: {:#?}",
-                    serde_json::to_string_pretty(&msg)
-                )
-            ),
+            _ => panic!("Unexpected message type"),
         }
     }
 }
 
 fn main() {
-    let _ = run(Echo::default());
+    run(EchoHandler, None);
 }
